6942037a0c033fc637f1d6752bc0c170e5835a7f
Thorben <bender.thorben0@gmail.com>
2020-10-19 13:50:02 +0200

    Add comments to index.js to organize imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+// Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+// Styling
+import './index.css';
+
+// Components
+import App from './App';
 
 ReactDOM.render(
   <Router>
