460d4c52f03b099d7370ee68bd1a7b2665f48f00
Thorben <bender.thorben0@gmail.com>
2020-10-21 12:02:39 +0200

    Add query to socket

--- src/Components/ChatRoom.js
+++ src/Components/ChatRoom.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 function ChatRoom(props) {
+  const socket = io('http://localhost:4000', {
+    query: {
+      room: props.match.params.roomname,
+      hasFoundCall: false,
+    },
+  });
+  socket.on('joined', () => {
+    console.log('Joined Room');
+  });
   return (
     <div>
-      <p>ChatRoom</p>
+      <p>ChatRoom {props.match.params.roomname}</p>
     </div>
   );
 }
